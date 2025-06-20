17978a253c501be28d3d7262935fb69b2d668561
suyashbha <suyashbhardwaj09@gmail.com>
2025-06-20 12:54:32 +0530

    edited the alowed cors origins

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://smart-stocks-frontend.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
